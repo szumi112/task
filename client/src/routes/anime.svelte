b2933d0c48f66ea50834c8ffdaac3c1a97be9c50
Mateusz <mattszumilo@gmail.com>
2024-09-04 19:38:46 +0200

    chore: initial styling

--- client/src/routes/anime.svelte
+++ client/src/routes/anime.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-    export let title: string;
-    export let mal_id: number;
-    export let image: string;
+  export let title: string;
+  export let mal_id: number;
+  export let image: string;
 </script>
 
-<h1>{title}</h1>
-
-<a href="/{mal_id}">
+<div class="anime-card  h-full bg-gray-800 rounded-md shadow-lg p-4"   >
+  <h1 class="text-lg font-bold text-white mb-2">{title}</h1>
+  <a href="/{mal_id}">
     <img
-        class="hover:opacity-50 hover:cursor-pointer"
-        src={image}
-        alt={title}
-        height="200px"
-        width="200px"
+      class="hover:opacity-80 hover:cursor-pointer rounded transition-all duration-300 ease-in-out"
+      src={image}
+      alt={title}
+      height="200px"
+      width="200px"
     />
-</a>
+  </a>
+</div>
